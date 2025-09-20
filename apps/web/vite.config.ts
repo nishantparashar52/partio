80640720e4b48050bbd4020308dc76d98c432de5
Nishant Parashar <nishant.parashar@tesco.com>
2025-09-21 04:04:43 +0530

    feat: added vitejs

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/partio/',
   plugins: [react()],
-  server: { port: 5173 }
+  build: {
+    outDir: 'dist'
+  }
+
 })
